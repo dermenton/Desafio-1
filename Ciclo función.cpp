8cb94a1b2b1ea4414ad816a49bd7571c1f5227cd
dermenton <a.munoz@udea.edu.co>
2024-09-16 03:03:15 -0500

    Con pulsador

--- Ciclo función.cpp
+++ Ciclo función.cpp
@@ -31,9 +31,45 @@ void loop()
   lcd_1.print(seconds);
   lcd_1.setBacklight(1);
   delay(100); // Wait for 500 millisecond(s)
-  lcd_1.setBacklight(0);
-  delay(100); // Wait for 500 millisecond(s)
-  seconds += 1;
+  //lcd_1.setBacklight(0);
+  //delay(100); // Wait for 500 millisecond(s)
+  seconds = seconds+1;
   
 }
+
+
+
+PULSADOR:
+
+const int StartPin = 2; // Pulsador 1 en pin A1
+const int StopPin = 3;  // Pulsador 2 en pin A2
+int estado2 = LOW;
+int estado3 = LOW;
+
+void setup() {
+    Serial.begin(9600);
+
+  // Configurar los pines de los pulsadores como entradas
+  pinMode(StartPin, INPUT);
+  pinMode(StopPin, INPUT);
+
+  // Iniciar la comunicación serial para ver los resultados
+}
+
+void loop() {
+  // Leer el estado de los pulsadores
+  estado2 = digitalRead(StartPin); // 1 si está presionado, 0 si no
+  estado3 = digitalRead(StopPin);   // 1 si está presionado, 0 si no
+
+  // Imprimir el estado de los pulsadores en el monitor serial
+  Serial.print("Pulsador 2 (2): ");
+  Serial.println(estado2); // Imprime 1 o 0 para el pulsador 1
+
+  Serial.print("Pulsador 3 (3): ");
+  Serial.println(estado3);  // Imprime 1 o 0 para el pulsador 2
+
+  delay(100); // Pequeño retraso de 100 ms para evitar lecturas excesivas
+}
+
+
 
