55197f43f0d969fcdf943bd0578e0a97230f99d5
dermenton <a.munoz@udea.edu.co>
2024-09-16 00:43:21 -0500

    Pantalla lcd actualizada

--- Ciclo función.cpp
+++ Ciclo función.cpp
@@ -1,44 +1,39 @@
-char receivedString[64]; // Se crea un string que sirve como buffer
-int stringIndex = 0;    // Index para recorrer el arreglo
-//Inicializando el LCD
-LiquidCrystal lcd (2,3,4,5,6,7);
-
-int analogPin = 0;
+/*int analogPin = 0;
 int val = 0;
+
 void setup()
 {
-  Serial.begin(9600); 
-    // Inicializa comunicación serial a unos baudios definidos
   Serial.begin(9600);
+}
+void loop()
+{
+  val = analogRead(analogPin);
+  Serial.println(val); 
+}
+*/
+//*
+#include <Adafruit_LiquidCrystal.h>
 
-  // Configurar número de filas y columnas del LCD
-  lcd.begin(8, 2);
+int seconds = 0;
 
+Adafruit_LiquidCrystal lcd_1(0);
 
+void setup()
+{
+  lcd_1.begin(16, 2);
+  
+  lcd_1.print("Ando Aprendiendoskii ");
 }
 
-void loop() {
-  val = analogRead(analogPin);
-	Serial.println(val);
-  // Revisar si hay datos disponibles por la conexión serial
-  if (Serial.available() > 0) {
-    // Se lee el caracter o los caracteres
-    char incomingChar = Serial.read();
-    //String testStr = Serial.readString();
+void loop()
+{
+  lcd_1.setCursor(0, 1);
+  lcd_1.print(seconds);
+  lcd_1.setBacklight(1);
+  delay(100); // Wait for 500 millisecond(s)
+  lcd_1.setBacklight(0);
+  delay(100); // Wait for 500 millisecond(s)
+  seconds += 1;
+  
+}
 
-    // Se almacena en el arreglo
-    receivedString[stringIndex] = incomingChar;
-    stringIndex++;
-    Serial.print(incomingChar);
-
-    // Revisar cuál es el caracter de cierre
-    if (incomingChar == '.') {
-      receivedString[stringIndex] = '\0'; // terminar en nulo
-      lcd.setCursor(0, 0);
-      lcd.print(receivedString); //Imprimir
-      //lcd.print(testStr);
-      stringIndex = 0; // Reset index
-    }
-  }
- 
-} 
