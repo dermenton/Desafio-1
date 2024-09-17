ef8420e0fa5b3df3eb63cbc98df0603fe2c1968c
dermenton <a.munoz@udea.edu.co>
2024-09-16 19:15:18 -0500

    Conexion de todo sin analisis

--- Ciclo función.cpp
+++ Ciclo función.cpp
@@ -1,41 +1,55 @@
-/*int analogPin = 0;
+/*//Click
+const int StartPin = 2; 
+const int StopPin = 4;  
+int estado1 = LOW;
+int estado2 = LOW;
+
+///Graficador
+int analogPin = 0;
 int val = 0;
 
-void setup()
-{
-  Serial.begin(9600);
-}
-void loop()
-{
-  val = analogRead(analogPin);
-  Serial.println(val); 
-}
-*/
-//*
+//LCD
 #include <Adafruit_LiquidCrystal.h>
 
-int seconds = 0;
 
+int seconds = 0;
 Adafruit_LiquidCrystal lcd_1(0);
 
-void setup()
+
+
+
+
+void setup() 
 {
+  Serial.begin(9600);
   lcd_1.begin(16, 2);
-  
-  lcd_1.print("Ando Aprendiendoskii ");
+  lcd_1.print("Hallaremos la");
+  lcd_1.setCursor(0, 1);
+  lcd_1.print("Frecuenciaa.");
 }
 
-void loop()
+void loop() 
 {
-  lcd_1.setCursor(0, 1);
-  lcd_1.print(seconds);
-  lcd_1.setBacklight(1);
-  delay(100); // Wait for 500 millisecond(s)
-  //lcd_1.setBacklight(0);
-  //delay(100); // Wait for 500 millisecond(s)
-  seconds = seconds+1;
   
-}
+  estado1 = digitalRead(StartPin); // 1 si está presionado, 0 si no
+  estado2 = digitalRead(StopPin);
+  
+  if (estado1 == 1)
+  {
+    while (estado2 == 0)
+      {
+        estado1 = digitalRead(StartPin);  
+        estado2 = digitalRead(StopPin);   
+        
+        val = analogRead(analogPin);
+        Serial.println(val); 
+  		
+      
+      } 
+  }
+ }
+ 
+ 
 
 
 
